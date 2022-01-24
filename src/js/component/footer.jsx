e4d1f6bbcb204693ab56f875feb8b3c1603b77a3
jseiglie <javierseiglie@gmail.com>
2022-01-24 16:13:27 +0000

    footer inserted

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+//import * as PropTypes from "prop-types";
+import "../../styles/footer.css";
+
+const FooterPage = () => {
+	return (
+		<>
+			<div className="row">
+				<div className="col-md-6">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Consequatur libero ipsum adipisci aut, ipsam nesciunt. Sunt
+					debitis enim odio sint, cupiditate deserunt cumque culpa
+					quam iusto aut, explicabo quis minima!
+				</div>
+				<div className="col-md-6">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Aperiam doloribus, consectetur a ipsam nobis voluptatum
+					tenetur? Deleniti doloremque, ex minima dolorem rem
+					voluptatibus id iusto incidunt facere distinctio ipsum
+					nobis.
+				</div>
+			</div>
+		</>
+	);
+};
+export default FooterPage;
